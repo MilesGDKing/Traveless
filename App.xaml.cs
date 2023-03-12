9dc87545168534f671cc8d4d96d4918e7eac4e8b
MilesGDKing <96638057+MilesGDKing@users.noreply.github.com>
2023-03-12 15:35:36 -0600

    not sure what to change, will continue work

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp14;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp14;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
